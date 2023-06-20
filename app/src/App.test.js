65b1bdf4c3dc1e140919557fd6dfeb7797708da4
M0Hassan <aminmohamedadan@gmail.com>
2023-06-20 21:16:44 +0300

    added tests

--- app/src/App.test.js
+++ app/src/App.test.js
@@ -1,7 +1,37 @@
-import { render, screen, fireEvent } from '@testing-library/react';
-import BookingForm from './components/BookingForm.js';
-import BookingPage from './components/BookingPage.js';
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import BookingPage, { initializeTimes, updateTimes } from './BookingPage';
 
-test("test the initializeTimes and updateTimes functions", () => {
-  
-})
+// Mock the fetchAPI function
+jest.mock('./api', () => ({
+  fetchAPI: jest.fn((date) => {
+    // Return mocked response based on date input
+    if (date === new Date().toDateString()) {
+      return ['10:00 AM', '11:00 AM', '12:00 PM'];
+    }
+    return [];
+  }),
+}));
+
+describe('BookingPage', () => {
+  test('initializeTimes should return available times for today', () => {
+    const availableTimes = initializeTimes();
+    expect(availableTimes).toEqual(['10:00 AM', '11:00 AM', '12:00 PM']);
+  });
+
+  test('updateTimes should return available times for the given date', () => {
+    const state = null;
+    const date = '2023-06-01';
+    const availableTimes = updateTimes(state, date);
+    expect(availableTimes).toEqual([]);
+  });
+
+  test('component renders correctly with available times', () => {
+    render(<BookingPage />);
+
+    // Assert the expected UI elements or behaviors based on the available times
+    expect(screen.getByText('10:00 AM')).toBeInTheDocument();
+    expect(screen.getByText('11:00 AM')).toBeInTheDocument();
+    expect(screen.getByText('12:00 PM')).toBeInTheDocument();
+  });
+});
